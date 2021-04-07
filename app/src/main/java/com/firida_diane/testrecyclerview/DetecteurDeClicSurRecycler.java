f8d44ab247bbd6934465d5a836fe5a503f87695a
diane <dianewoulsou@gmail.com>
2021-04-07 15:49:59 +0200

    Initial commit

---
+++ app/src/main/java/com/firida_diane/testrecyclerview/DetecteurDeClicSurRecycler.java
@@ -0,0 +1,10 @@
+package com.firida_diane.testrecyclerview;
+
+import android.view.View;
+
+public interface DetecteurDeClicSurRecycler {
+
+    public void clicSurRecyclerItem(int position, View v);
+
+
+}
